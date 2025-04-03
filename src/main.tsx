b9deddf6a280790f570af6d67b756a2987aeded1
Myrzabek Kaisar <myrzabekkajsar89@gmail.com>
2025-04-03 16:55:00 +0500

    ergrt

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./homework/29.2/add.tsx";
+import App from "./proekt/add.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
